92c5ca866a13fc266dbb5aa9b0a7d18bfec1116f
Archisman Roy <royarchi31@gmail.com>
2020-02-15 23:56:53 -0800

    group rolling

--- toolbox/pandas_utility.py
+++ toolbox/pandas_utility.py
@@ -49,3 +49,32 @@ def stringify_columns(df,column_list):
         df_copy[i] = df_copy[i].astype(str)
         
     return df_copy
+
+def group_rolling_min(data, group_by_cols, time_col, shift_val, window_val, min_period_val, metric_col, metric_name):
+    """
+    data: Input dataframe
+    group_by_cols: List of columns used to group by
+    time_col: The columns against whom the data should be sorted, in most cases this will be datetime column
+    shift_val: How much shifting is required for appropriate lead or lag
+    window_val: rolling window
+    min_period_val: Decides the minimum number of elements required in a window for computation
+    metric_col: Column name where rolling will be applied
+    metric_name: Name of output column
+    """
+    df = data.copy()
+    
+    #Sort
+    df.sort_values(by=group_by_cols + [time_col])
+    
+    #Shift if needed
+    df['shifted'] = df.groupby(group_by_cols)[metric_col].shift(shift_val)
+
+    #Build reset list
+    reset_list = []
+    for i in range(len(group_by_cols)):
+        reset_list.append(i)
+    
+    #Rolling
+    df[metric_name] = df.groupby(group_by_cols)['shifted'].rolling(window=window_val,min_periods = min_period_val).min().reset_index(reset_list,drop=True)
+    
+    return df 
