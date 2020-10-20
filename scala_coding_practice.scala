a3ff2a33878a1311cd8327eb0b35124e828d2ada
Archisman Roy <43758396+Archisman-Roy@users.noreply.github.com>
2020-10-19 21:56:33 -0700

    Parellel Scala ops

--- scala_coding_practice.scala
+++ scala_coding_practice.scala
@@ -1,8 +1,17 @@
+// Code start
+
 // Reading data
 // Source Github link
 // https://github.com/MarkCLewis/BigDataAnalyticswithSpark/blob/master/src/main/scala/standardscala/TempData.scala
 
 package standardScala
+
+// Add dependency in sbt file 
+// libraryDependencies += "org.scala-lang.modules" %% "scala-parallel-collections" % "0.2.0"
+// Reference stackoverflow link
+// https://stackoverflow.com/questions/56542568/missing-import-scala-collection-parallel-in-scala-2-13
+
+import scala.collection.parallel.CollectionConverters._
 
 case class TempData(day: Int, doy: Int, month: Int, year: Int,
                     precip: Double, snow: Double, tave:Double, tmax: Double, tmin: Double)
@@ -53,5 +62,27 @@ object TempData {
    m -> days.foldLeft(0.0)((sum, td) => sum+td.tmax)/days.length
   }
   monthlyTemp.toSeq.sortBy(_._1) foreach println
+   
+  // This is the parellel version of FoldLeft. The output is same as obtained from FoldLeft
+  // Parellel operations work only on associative functions like addition
+  // Parellel operations fail on operation like subtraction since subtraction is not associative
+  // aggregate takes in 2 ops: sequential ops and combination ops
+  // sequential ops is the same operation of FoldLeft
+  // Output of sequential operation is combined in combination operation
+  // Combination ops fails to produce right output if the sequential ops is not associative
+   
+  val (rainySum_par, rainyCount2_par) = data.par.aggregate(0.0 -> 0)({ case ((sum, cnt), td) =>
+   if(td.precip < 1.0) (sum, cnt) else (sum+td.tmax, cnt+1)
+  }, { case ((s1, c1), (s2, c2)) =>
+   (s1+s2, c1+c2)
+  })
+  println(s"Average Rainy temp is ${rainySum_par/rainyCount2_par}") 
+   
+   
  }
 }
+
+// Code end
+
+// Code start
+// Code end
