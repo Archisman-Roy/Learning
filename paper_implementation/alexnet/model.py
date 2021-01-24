56c87837b83c7a1f7bb4cb31a55eb85ee7b3e2f1
Archisman-Roy <royarchi31@gmail.com>
2021-01-24 07:59:30 +0000

    alexnet setup

---
+++ paper_implementation/alexnet/model.py
@@ -0,0 +1,115 @@
+"""
+Implementation of AlexNet, from paper
+"ImageNet Classification with Deep Convolutional Neural Networks" by Alex Krizhevsky et al.
+See: https://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf
+Source code: https://github.com/dansuh17/alexnet-pytorch/blob/d0c1b1c52296ffcbecfbf5b17e1d1685b4ca6744/model.py#L40
+"""
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils import data
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from tensorboardX import SummaryWriter
+
+# define pytorch device - useful for device-agnostic execution
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# define model parameters
+NUM_EPOCHS = 90  # original paper
+BATCH_SIZE = 128
+MOMENTUM = 0.9
+LR_DECAY = 0.0005
+LR_INIT = 0.01
+IMAGE_DIM = 227  # pixels
+NUM_CLASSES = 1000  # 1000 classes for imagenet 2012 dataset
+DEVICE_IDS = [0, 1, 2, 3]  # GPUs to use
+# modify this to point to your data directory
+INPUT_ROOT_DIR = 'data/'
+TRAIN_IMG_DIR = 'data/alexnet/in'
+OUTPUT_DIR = 'data/alexnet/out'
+LOG_DIR = OUTPUT_DIR + '/tblogs'  # tensorboard logs
+CHECKPOINT_DIR = OUTPUT_DIR + '/models'  # model checkpoints
+
+# make checkpoint path directory
+os.makedirs(CHECKPOINT_DIR, exist_ok=True)
+
+
+class AlexNet(nn.Module):
+    """
+    Neural network model consisting of layers propsed by AlexNet paper.
+    """
+    def __init__(self, num_classes=1000):
+        """
+        Define and allocate layers for this neural net.
+        Args:
+            num_classes (int): number of classes to predict with this model
+        """
+        super().__init__()
+        # input size should be : (b x 3 x 227 x 227)
+        # The image in the original paper states that width and height are 224 pixels, but
+        # the dimensions after first convolution layer do not lead to 55 x 55.
+        self.net = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4),  # (b x 96 x 55 x 55)
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),  # section 3.3
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 96 x 27 x 27)
+            nn.Conv2d(96, 256, 5, padding=2),  # (b x 256 x 27 x 27)
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 256 x 13 x 13)
+            nn.Conv2d(256, 384, 3, padding=1),  # (b x 384 x 13 x 13)
+            nn.ReLU(),
+            nn.Conv2d(384, 384, 3, padding=1),  # (b x 384 x 13 x 13)
+            nn.ReLU(),
+            nn.Conv2d(384, 256, 3, padding=1),  # (b x 256 x 13 x 13)
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 256 x 6 x 6)
+        )
+        # classifier is just a name for linear layers
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=(256 * 6 * 6), out_features=4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=4096, out_features=4096),
+            nn.ReLU(),
+            nn.Linear(in_features=4096, out_features=num_classes),
+        )
+        self.init_bias()  # initialize bias
+        
+    def init_bias(self):
+        for layer in self.net:
+            if isinstance(layer, nn.Conv2d):
+                nn.init.normal_(layer.weight, mean=0, std=0.01)
+                nn.init.constant_(layer.bias, 0)
+        # original paper = 1 for Conv2d layers 2nd, 4th, and 5th conv layers
+        nn.init.constant_(self.net[4].bias, 1)
+        nn.init.constant_(self.net[10].bias, 1)
+        nn.init.constant_(self.net[12].bias, 1)
+
+    def forward(self, x):
+        """
+        Pass the input through the net.
+        Args:
+            x (Tensor): input tensor
+        Returns:
+            output (Tensor): output tensor
+        """
+        x = self.net(x)
+        x = x.view(-1, 256 * 6 * 6)  # reduce the dimensions for linear layer input
+        return self.classifier(x)
+
+        
+if __name__ == '__main__':
+    # create dataset and data loader
+    dataset = datasets.ImageFolder(TRAIN_IMG_DIR, transforms.Compose([
+        # transforms.RandomResizedCrop(IMAGE_DIM, scale=(0.9, 1.0), ratio=(0.9, 1.1)),
+        transforms.CenterCrop(IMAGE_DIM),
+        # transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]))
+    print('Dataset created')
