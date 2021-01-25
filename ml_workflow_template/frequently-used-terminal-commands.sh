6e03e5211b3cbcde12dbe75cd2b25e2518bd95a6
Archisman-Roy <royarchi31@gmail.com>
2021-01-25 07:20:47 +0000

    clean up

---
+++ ml_workflow_template/frequently-used-terminal-commands.sh
@@ -0,0 +1,7 @@
+cp -R path_to_source path_to_destination # to copy
+
+find . -name "*.DS_Store" -type f # to find files of particular extension recursively
+
+find . -name "*.DS_Store" -type f -delete # to delete files of particular extension recursively
+
+man command-name # to read user manual
